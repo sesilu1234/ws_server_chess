eed3a86e92c5f424cf463914ac6424cf9ca6246e
UlisesPlaRocher <ulisesplarocher@gmail.com>
2024-11-24 20:39:27 +0100

    yasdasdsdaasd

--- ws_chess_2.js
+++ ws_chess_2.js
@@ -48,11 +48,10 @@ dotenv.config();
  
      const currentTime = Date.now();
  
-     for (let element of timer_games_plus10) {  
-      console.log(element);
-      console.log(element[element.currentplayer] );
-      console.log(element.timestart );
-      console.log(currentTime );
+     for (let [key, element] of timer_games_plus10) {  
+      
+
+      
 
  
         console.log(element[element.currentplayer] - (currentTime - element.timestart) );
