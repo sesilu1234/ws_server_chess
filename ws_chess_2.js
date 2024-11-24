f0760ba0bc797b86bd458e351e176a875c5cc1fc
UlisesPlaRocher <ulisesplarocher@gmail.com>
2024-11-24 12:48:45 +0100

    yasdasdsda

--- ws_chess_2.js
+++ ws_chess_2.js
@@ -264,14 +264,14 @@ wss.on("connection", (ws) => {
         const game_sql = await promisePool.query(sql_id, [
           payload.id,
           
-        ]);
+        ])[0];
 
         
-        const match_rows = game_sql[0]; // probar que pasa si es vacio, y como comprobarlo, creo hay que coger game_sql[0][0]
-        console.log("eied", match_rows);
-          if (match_rows) {
+       // probar que pasa si es vacio, y como comprobarlo, creo hay que coger game_sql[0][0]
+        console.log("eied", game_sql);
+          if (game_sql) {
 
-           const match = match_rows[0];
+           const match = game_sql[0];
 
             const game = games.get(payload.id); // Retrieve the game object by its ID
 
