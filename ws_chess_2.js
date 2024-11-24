4e39dcf738fae626e5fce2795090cae1786ec32a
UlisesPlaRocher <ulisesplarocher@gmail.com>
2024-11-24 23:11:41 +0100

    yasdasdsdaasd

--- ws_chess_2.js
+++ ws_chess_2.js
@@ -2,165 +2,111 @@ const fs = require("fs");
 const https = require("https");
 const WebSocket = require("ws");
 const mysql = require("mysql2");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 // Load environment variables from .env file
-dotenv.config(); 
+dotenv.config();
 
+// For modality time_per_player :
 
+// when a player moves his time left is updated to as the time when his movement started in server side but also sent to client(maybe better to send this when his turn begins);so when he moves (so start_movement must be stored0),
 
+// For modality time_per_round :
 
+// when a player moves his time resets to original
 
+// El ID ha de ser generado por servers
 
+// al enviar movimiento, delete de minus10, siempre...eso si, primero cambio el undefined y despues deleteo en mins10, no vaya a ser
 
+const timer_games_plus10 = new Map();
+const timer_games_minus10 = new Map();
 
+//    algorithmic_max_time  =>     10.000 + 1 + var_exec + exec_time
 
+const per_player_plus10 = () => {
+  const currentTime = Date.now();
 
+  for (let [key, element] of timer_games_plus10) {
+    console.log(timer_games_plus10);
 
+    if (
+      element[element.currentplayer].time - (currentTime - element.timestart) <
+      15000
+    ) {
+      timer_games_minus10.set(key, { ...element });
+      element.timestart = undefined;
+    }
+  }
+};
 
- // For modality time_per_player :
-
-      // when a player moves his time left is updated to as the time when his movement started in server side but also sent to client(maybe better to send this when his turn begins);so when he moves (so start_movement must be stored0),
-      
-     
-
-
- // For modality time_per_round :
-
-      // when a player moves his time resets to original 
- 
-
- // El ID ha de ser generado por servers
-
-
- // al enviar movimiento, delete de minus10, siempre...eso si, primero cambio el undefined y despues deleteo en mins10, no vaya a ser
-
- const timer_games_plus10 = new Map();
- const timer_games_minus10 = new Map();
-
- 
- 
-  //    algorithmic_max_time  =>     10.000 + 1 + var_exec + exec_time
- 
- const per_player_plus10 = () => {
-
-  
- 
-     const currentTime = Date.now();
- 
-     for (let [key, element] of timer_games_plus10) {  
-    
-      console.log(timer_games_plus10);
- 
-       
-       
-         if (element[element.currentplayer].time - (currentTime - element.timestart) < 15000) {
-             timer_games_minus10.set(key, { ...element });
-             element.timestart = undefined;
- 
- 
-         }
-     }
- };
- 
- const per_player_minus10 = () => {
-
+const per_player_minus10 = () => {
   console.log(timer_games_minus10);
- 
-     const currentTime = Date.now();
- 
-     for (let [key, element] of timer_games_minus10) {  
- 
-       
-         if (element[element.currentplayer].time - (currentTime - element.timestart) < -0.2) {
-             
-             
-             
- 
-             element.player1.client.send(
-                 JSON.stringify({ type: "end_game", payload: {motive : "end_time", currentplayer: element[element.currentplayer].name } })
-               );
-               element.player2.client.send(
-                 JSON.stringify({ type: "end_game", payload: {motive : "end_time", currentplayer: element[element.currentplayer].name } })
-               );
 
-               timer_games_minus10.delete(key);
-               timer_games_plus10.delete(key);
+  const currentTime = Date.now();
 
-         }
-     }
- };
- 
- 
- 
- 
- 
- 
- 
- setInterval(() => { per_player_plus10() }, 10000);
- 
- setInterval(() => { per_player_minus10() }, 1000);
- 
- 
- 
+  for (let [key, element] of timer_games_minus10) {
+    if (
+      element[element.currentplayer].time - (currentTime - element.timestart) <
+      -0.2
+    ) {
+      element.player1.client.send(
+        JSON.stringify({
+          type: "end_game",
+          payload: {
+            motive: "end_time",
+            currentplayer: element[element.currentplayer].name,
+          },
+        })
+      );
+      element.player2.client.send(
+        JSON.stringify({
+          type: "end_game",
+          payload: {
+            motive: "end_time",
+            currentplayer: element[element.currentplayer].name,
+          },
+        })
+      );
 
+      timer_games_minus10.delete(key);
+      timer_games_plus10.delete(key);
+    }
+  }
+};
 
+setInterval(() => {
+  per_player_plus10();
+}, 10000);
 
+setInterval(() => {
+  per_player_minus10();
+}, 1000);
 
-
-
-
-
-
-
-
- function getRandomInt() {
+function getRandomInt() {
   return Math.floor(Math.random() * 10);
 }
 
 function getRandomLetter() {
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   return letters.charAt(Math.floor(Math.random() * letters.length));
 }
 
 function getRandomID() {
-     return id = getRandomLetter() + getRandomInt() + "-" +
-       getRandomLetter() + getRandomLetter() + getRandomInt() + getRandomLetter() + "-" +
-       getRandomLetter() + getRandomLetter() + getRandomInt() + getRandomInt();
-
-
+  return (id =
+    getRandomLetter() +
+    getRandomInt() +
+    "-" +
+    getRandomLetter() +
+    getRandomLetter() +
+    getRandomInt() +
+    getRandomLetter() +
+    "-" +
+    getRandomLetter() +
+    getRandomLetter() +
+    getRandomInt() +
+    getRandomInt());
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -175,7 +121,6 @@ const pool = mysql.createPool({
 // Optional: Promisify the pool for async/await support
 const promisePool = pool.promise();
 
-
 // Read the SSL certificate files from the certbot folder
 const server = https.createServer({
   cert: fs.readFileSync("../certbot/fullchain.pem"),
@@ -185,33 +130,20 @@ const server = https.createServer({
 // Create a secure WebSocket server on top of the HTTPS server
 const wss = new WebSocket.Server({ server });
 
-
 let clients = new Array(); // Store all connected ips
 
 let games = new Map(); // Store all connected games
-
 
 wss.on("connection", (ws) => {
   clients.push(ws);
   console.log("Client connected. Total games:", clients.length);
 
-  
-
   ws.on("message", async (data) => {
     try {
-
-
-
       const message = JSON.parse(data);
       const payload = message.payload;
 
       switch (message.type) {
-
-
-
-
-
-
         case "create_game":
           const now = new Date();
           const isoString = now.toISOString();
@@ -230,8 +162,7 @@ wss.on("connection", (ws) => {
               payload.color2,
               payload.countdown_time,
               payload.time_modality,
-              time
-              
+              time,
             ]);
 
             console.log("Game inserted successfully with ID:", id);
@@ -239,56 +170,37 @@ wss.on("connection", (ws) => {
             console.error("Error inserting game:", error);
           }
 
+          games.set(id, {
+            player1: {
+              client: ws,
+              name: payload.player1,
+              time: payload.countdown_time,
+            },
+            player2: {
+              client: undefined,
+              name: payload.player2,
+              time: payload.countdown_time,
+            },
+            time_modality: payload.time_modality,
+            currentplayer: "player1",
+            timestart: undefined,
+          });
 
-
-          
-        games.set(id, {
-          player1: {
-            client: ws,
-            name: payload.player1,
-            time: payload.countdown_time
-          },
-          player2: {
-            client: undefined,
-            name: payload.player2,
-            time: payload.countdown_time
-          },
-          time_modality: payload.time_modality,
-          currentplayer: "player1",
-          timestart: undefined
-        });
-        
-          
-        ws.send(
-          JSON.stringify({ type: "ID", payload: id })
-        );
-
-
+          ws.send(JSON.stringify({ type: "ID", payload: id }));
 
           break;
 
         case "join_game":
-      
+          const sql_id = "SELECT * FROM created_games WHERE id = ?";
 
-        const sql_id = "SELECT * FROM created_games WHERE id = ?";
+          const game_sql = await promisePool.query(sql_id, [payload.id]);
 
-        const game_sql = await promisePool.query(sql_id, [
-          payload.id,
-          
-        ]);
-
-      
-        
           if (game_sql[0].length > 0) {
-
-           const match = game_sql[0][0];
+            const match = game_sql[0][0];
 
             const game = games.get(payload.id); // Retrieve the game object by its ID
 
-           
-
             game.player2.client = ws;
-            
 
             const sendJSON1 = {
               id: match.id,
@@ -311,11 +223,6 @@ wss.on("connection", (ws) => {
               round: 2,
             };
 
-
-
-           
-
-
             game.player1.client.send(
               JSON.stringify({ type: "start_game", payload: sendJSON1 })
             );
@@ -325,11 +232,8 @@ wss.on("connection", (ws) => {
 
             game.timestart = Date.now();
 
-
-            const x = [match.id, game]; 
-            timer_games_plus10.set(x[0], x[1]);    
-            
-
+            const x = [match.id, game];
+            timer_games_plus10.set(x[0], x[1]);
           } else {
             ws.send(
               JSON.stringify({
@@ -341,14 +245,13 @@ wss.on("connection", (ws) => {
           break;
 
         case "move":
-
           const game = games.get(payload.id);
 
-          if (game.time_modality == "perPlayer")   {   game[game.currentplayer].time -= ((Date.now() - game.timestart))      }
-
+          if (game.time_modality == "perPlayer") {
+            game[game.currentplayer].time -= Date.now() - game.timestart;
+          }
 
           console.log(game);
-
 
           const sendJSON = {
             id: payload.id,
@@ -357,11 +260,11 @@ wss.on("connection", (ws) => {
             turn: payload.turn,
             pawn_promotion: payload.pawn_promotion,
             castling: payload.castle,
-            time_left: game.currentplayer === "player1" ? game.player2.time : game.player1.time
+            time_left:
+              game.currentplayer === "player1"
+                ? game.player2.time
+                : game.player1.time,
           };
-          
-         
-
 
           if (game.player1.client === ws) {
             game.player2.client.send(
@@ -375,41 +278,24 @@ wss.on("connection", (ws) => {
               JSON.stringify({ type: "move", payload: sendJSON })
             );
             game.player2.client.send(
-                JSON.stringify({ type: "time_left", payload: sendJSON.time_left })
+              JSON.stringify({ type: "time_left", payload: sendJSON.time_left })
             );
           }
 
-          game.currentplayer = game.currentplayer === "player1" ? "player2" : "player1"
-          
-          game.timestart = Date.now();  
+          game.currentplayer =
+            game.currentplayer === "player1" ? "player2" : "player1";
+
+          game.timestart = Date.now();
 
           timer_games_minus10.delete(payload.id);
 
           break;
-          
 
-
-          case "recover":
-          
+        case "recover":
           break;
 
-
-          case "talk":
-          
+        case "talk":
           break;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         default:
           console.log("Unknown action:", message.type);
@@ -420,26 +306,10 @@ wss.on("connection", (ws) => {
     }
   });
 
-
-
-ws.on("close", () => {
-  
-  
-  
-
-
-
-
-  console.log("Client gone, now games and games length is: ", games.length);
-
-
+  ws.on("close", () => {
+    console.log("Client gone, now games and games length is: ", games.length);
   });
-
-  
-  
-
 });
-  
 
 // Start the HTTPS server
 server.listen(8080, () => {
