b51acee3993e262ab6cf2817cb62f7e19f6af573
UlisesPlaRocher <ulisesplarocher@gmail.com>
2025-01-14 23:05:43 +0100

    asjdsa

--- ws_chess_2.js
+++ ws_chess_2.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const https = require("https");
+const http = require("http");
 const WebSocket = require("ws");
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
